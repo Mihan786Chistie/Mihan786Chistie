5aa4ac0c7bd266e0bb14b6cc782d295f6cc5f890
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2024-07-26 21:18:12 +0530

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,17 +1,17 @@
 import requests
 import re
 
-# Fetch a random meme URL from an API
-response = requests.get("https://meme-api.com/gimme/programmerhumour")
-data = response.json()
-try:
-    meme_url = data['preview'][3]
-except:
-    meme_url = data['preview'][2]
-finally:
-    meme_url = data['preview'][1]
-
-print(meme_url)
+def request_meme():
+    while True:
+        try:
+            response = requests.get("https://meme-api.com/gimme/programmerhumour")
+            data = response.json()
+            meme_url = data['preview'][3]
+            return meme_url
+        except:
+            pass
+    
+meme_url = request_meme()
 
 f = open("README.md", "w")
 
